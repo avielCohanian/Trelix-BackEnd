fe433052b21eb3642d7146953ab32bed4204a171
Aviel Cohanian <avial151515@gmail.com>
2021-12-30 22:57:45 +0200

    backend-Trelix

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dbURL: 'mongodb+srv://aviel15:123zaq@cluster0.kwwlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+    // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+};
