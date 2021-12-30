204c1daec1f087920ec886dfe68a9aa85d0e91da
avielCohanian <91903385+avielCohanian@users.noreply.github.com>
2021-12-30 23:17:56 +0200

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,3 @@
 module.exports = {
-    dbURL: 'mongodb+srv://aviel15:123zaq@cluster0.kwwlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-
-    // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+     dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
 };
